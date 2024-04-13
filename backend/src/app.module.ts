2d064494209bed1a338d5ef63bcbf09b8dfff8f1
skykhs3 <skykhs3@naver.com>
2024-04-13 18:35:20 +0900

    frontend docker-compose 구성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST || "localhost",
+      port: 3306,
+      username: process.env.DB_USERNAME || "root",
+      password: process.env.DB_PASSWORD || "your_password",
+      database: process.env.DB_DATABASE || "some_db",
+      synchronize: true,
+      logging: false,
+      entities: [
+        "src/entity/**/*.ts"
+      ],
+      migrations: [
+        "src/migration/**/*.ts"
+      ],
+      subscribers: [
+        "src/subscriber/**/*.ts"
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 800, // 매 초 변경사항을 확인
+            aggregateTimeout: 300, // 재빌드 전 지연 시간
+        };
+        return config;
+    },
+};
 
 export default nextConfig;
