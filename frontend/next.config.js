5da28a79c7727f89d6d9607d82f1a8cce2344193
skykhs3 <skykhs3@naver.com>
2024-05-18 16:55:23 +0900

    depreciated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpackDevMiddleware: (config) => {
+  swcMinify: true,
+  // except for webpack, other parts are left as generated
+  webpack: (config, context) => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
