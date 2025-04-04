b78b54c8f3397404b3340519e1dfafbc344511dc
skykhs3 <skykhs3@naver.com>
2025-04-04 16:06:55 +0900

    chore

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 bg-white dark:bg-secondary-950">
+      <main className="flex-1">
         {/* Hero Banner */}
         <Banner />
 
